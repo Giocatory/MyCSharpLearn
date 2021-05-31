a9025a3843c9855ca83e89a5ae6ee6da8cb28ef1
Giocatory <nivolyas@gmail.com>
2021-05-31 18:44:18 +0300

    31.05.2021

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -6,8 +6,25 @@ namespace LessonOne
     {
         static void Main(String[] args)
         {
-            Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess());
+            Console.WriteLine($"System process: {System.Diagnostics.Process.GetCurrentProcess()}");
+            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
+            Console.WriteLine($"Current Managed Thread ID: {Environment.CurrentManagedThreadId}");
+            Console.WriteLine("Logical Drives: ");
+            GetDrivers();
+            Console.WriteLine($"System is 64bit?: {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"OS version: {Environment.OSVersion}");
+            Console.WriteLine($"Machine name: {Environment.MachineName}");
+            Console.WriteLine($".NET version: {Environment.Version}");
+            Console.WriteLine($"System directory: {Environment.SystemDirectory}");
             Console.ReadKey();
+        }
+        static void GetDrivers()
+        {
+            String[] drivers = Environment.GetLogicalDrives();
+            foreach (var drive in drivers)
+            {
+                Console.WriteLine(drive);
+            }
         }
     }
 }
