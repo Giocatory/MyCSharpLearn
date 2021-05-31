c7f1b101bcc6a98ecbb9fc4768e62f9963272ca3
Giocatory <nivolyas@gmail.com>
2021-05-31 19:50:25 +0300

    31.05.21

--- LessonThree/Program.cs
+++ LessonThree/Program.cs
@@ -6,7 +6,7 @@ namespace LessonThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
