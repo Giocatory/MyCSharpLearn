989da608ade64b9d826bd1e762989bc6a59e54ea
Giocatory <nivolyas@gmail.com>
2021-05-31 11:06:21 +0300

    Добавьте файлы проекта.

---
+++ LessonOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LessonOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LessonTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LessonTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
