ce794077890ddcb499f20f59200d68aa42a65036
Giocatory <nivolyas@gmail.com>
2021-06-01 10:01:02 +0300

    01.06.21

--- LessonThree/Program.cs
+++ LessonThree/Program.cs
@@ -6,7 +6,20 @@ namespace LessonThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            string s1 = "Hello";
+            string s2 = "Mikle";
+            string s3 = s1 + " " + s2;
+
+            Console.WriteLine(s3);
+            Console.WriteLine(s1 == s2);
+            Console.WriteLine(s1.Equals(s2));
+            Console.WriteLine(s1.Equals(s1));
+            Console.WriteLine("hello".Equals(s1));
+            Console.WriteLine(s1 == "hello");
+            Console.WriteLine("hello".ToUpper().Equals(s1.ToUpper()));
+            Console.WriteLine(s1.Equals("hello", StringComparison.CurrentCultureIgnoreCase));
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
