1e6d03559d6a2bac0a2f373bf3479c8c939e3a2f
Giocatory <nivolyas@gmail.com>
2021-06-01 10:34:17 +0300

    01.06.21

---
+++ LessonFour/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LessonFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 4, 5, 6, 7, 8 };
+            // LINQ query
+            var subset = from i in numbers where i < 11 select i; // var == System.Collections.Generic.IEnumerable<int>
+
+            foreach (var k in subset)
+            {
+                Console.Write($"{k} "); // 10 1 2 3 4 5 6 7 8
+            }
+            Console.WriteLine();
+            Console.WriteLine(subset.GetType().Name); // WhereArrayIterator`1
+            Console.WriteLine(subset.GetType().Namespace); // System.Linq
+            Console.WriteLine(subset.GetType().BaseType); // System.Linq.Enumerable+Iterator`1[System.Int32]
+        }
+    }
+}
--- LessonThree/Program.cs
+++ LessonThree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace LessonThree
 {
@@ -10,16 +11,35 @@ namespace LessonThree
             string s2 = "Mikle";
             string s3 = s1 + " " + s2;
 
-            Console.WriteLine(s3);
-            Console.WriteLine(s1 == s2);
-            Console.WriteLine(s1.Equals(s2));
-            Console.WriteLine(s1.Equals(s1));
-            Console.WriteLine("hello".Equals(s1));
-            Console.WriteLine(s1 == "hello");
-            Console.WriteLine("hello".ToUpper().Equals(s1.ToUpper()));
-            Console.WriteLine(s1.Equals("hello", StringComparison.CurrentCultureIgnoreCase));
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine(s3); // Hello Mikle
+            Console.WriteLine(s1 == s2); // false
+            Console.WriteLine(s1.Equals(s2)); // false
+            Console.WriteLine(s1.Equals(s1)); //true
+            Console.WriteLine("hello".Equals(s1)); // false
+            Console.WriteLine(s1 == "hello"); // false
+            Console.WriteLine("hello".ToUpper().Equals(s1.ToUpper())); //true
+            Console.WriteLine(s1.Equals("hello", StringComparison.CurrentCultureIgnoreCase)); //true
+
+            //################################################################################
+
+            Console.WriteLine("\nString Builder");
+            StringBuilder sb = new StringBuilder("### Hi Mikhail ###");
+            sb.Append("\n");
+            sb.AppendLine("World of Warcraft...");
+            sb.AppendLine("Albion");
+            sb.AppendLine("Aion 2");
+            sb.AppendLine("Lineage 2");
+            Console.WriteLine(sb.ToString());
+            sb.Replace("2", "God in me!"); // looking for "2" in the entire row and if it finds, then replaces the value everywhere
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine($"sb length - {sb.Length}"); // 66
+
+            //################################################################################
+            Console.WriteLine("\nDynamic variable");
+            dynamic variable = 15;
+            Console.WriteLine(variable);
+            variable = "Mikhail";
+            Console.WriteLine(variable);
         }
     }
 }
