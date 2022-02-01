daf341873bb8996e9f6e9ab827331f05cac84f92
Giocatory <nivolyas@gmail.com>
2022-02-01 10:47:00 +0300

    based nullable types

---
+++ LessonEight/Program.cs
@@ -0,0 +1,14 @@
+﻿// Nullable data types
+// If you put the sign "?", then it is "possible" that the variable has the value "null"
+string? str = null;
+object? obj = null;
+int? num = default;
+int? n = null;
+int? nu = 1;
+
+
+Console.WriteLine(str == null ? "null" : "not null"); // null
+Console.WriteLine(obj == null ? "null" : "not null"); // null
+Console.WriteLine(num == null ? "null" : "not null"); // null
+Console.WriteLine(n == null ? "null" : "not null"); // null
+Console.WriteLine(nu == null ? "null" : "not null"); // not null
